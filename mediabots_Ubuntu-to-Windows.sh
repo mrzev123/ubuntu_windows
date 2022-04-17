d8fbf8fae9b668a989a9383cfb4899c3a6d542fb
mrzev123 <39166589+mrzev123@users.noreply.github.com>
2022-04-17 22:27:25 +0800

    Update mediabots_Ubuntu-to-Windows.sh

--- mediabots_Ubuntu-to-Windows.sh
+++ mediabots_Ubuntu-to-Windows.sh
@@ -32,13 +32,13 @@ fi
 sudo ln -s /usr/bin/genisoimage /usr/bin/mkisofs
 # Downloading resources
 sudo mkdir /mediabots /floppy /virtio
-link1_status=$(curl -Is https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso | grep HTTP | cut -f2 -d" " | head -1)
-link2_status=$(curl -Is https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso | grep HTTP | cut -f2 -d" ")
+link1_status=$(curl -Is https://archive.org/download/WS2019_trial/WS2019.iso | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is https://archive.org/download/WS2019_trial/WS2019.iso | grep HTTP | cut -f2 -d" ")
 #sudo wget -P /mediabots https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso # Windows Server 2012 R2 
 if [ $link1_status = "200" ] ; then 
-	sudo wget -O /mediabots/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso 
+	sudo wget -O /mediabots/WS2019.iso https://archive.org/download/WS2019_trial/WS2019.iso 
 elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
-	sudo wget -P /mediabots https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso
+	sudo wget -P /mediabots https://archive.org/download/WS2019_trial/WS2019.iso
 else
 	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows OS image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
 	echo "Exiting.."
